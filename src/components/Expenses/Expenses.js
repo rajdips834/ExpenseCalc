e0690255a6d982b06ceec853345446c737399973
Rajdip Sinha <rajdipsinha@Anirbans-MacBook-Pro.local>
2023-08-02 20:32:42 +0530

    Adding task and sorting year wise added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,20 +7,23 @@ import "./Expenses.css";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
-
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
-
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter
+          yearList={props.items}
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
+        {filteredExpenses.map((expense) => (
           <ExpenseItem
+            key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import './ExpensesFilter.css';
+import "./ExpensesFilter.css";
 
 const ExpensesFilter = (props) => {
   const dropdownChangeHandler = (event) => {
     props.onChangeFilter(event.target.value);
   };
-
+  const distinct = (value, index, self) => {
+    return self.indexOf(value) === index;
+  };
+  var yearList = [];
+  props.yearList.map((expense) => {
+    yearList.push(expense.date.getFullYear().toString());
+    yearList = yearList.filter(distinct);
+  });
+  console.log(yearList);
   return (
-    <div className='expenses-filter'>
-      <div className='expenses-filter__control'>
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
         <label>Filter by year</label>
         <select value={props.selected} onChange={dropdownChangeHandler}>
-          <option value='2022'>2022</option>
-          <option value='2021'>2021</option>
-          <option value='2020'>2020</option>
-          <option value='2019'>2019</option>
+          {yearList.map((year) => (
+            <option value={year}>{year}</option>
+          ))}
         </select>
       </div>
     </div>
